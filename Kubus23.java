48521120b51703a8699ab30090232baa8750f254
Yann <yann@gmail.com>
2025-12-04 21:11:45 +0700

    Tugas 1 Revisi

---
+++ Kubus23.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Kubus23 {
+
+    // Fungsi menghitung volume kubus (bilangan bulat)
+    public static int hitungVolume(int sisi) {
+        return sisi * sisi * sisi;
+    }
+
+    // Fungsi menghitung luas permukaan kubus (bilangan bulat)
+    public static int hitungLuasPermukaan(int sisi) {
+        return 6 * sisi * sisi;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan panjang sisi kubus: ");
+        int sisi = sc.nextInt();
+
+        int volume = hitungVolume(sisi);
+        int luasPermukaan = hitungLuasPermukaan(sisi);
+
+        System.out.println("\n=== HASIL PERHITUNGAN KUBUS ===");
+        System.out.println("Volume kubus: " + volume);
+        System.out.println("Luas permukaan kubus: " + luasPermukaan);
+
+        sc.close();
+    }
+}
